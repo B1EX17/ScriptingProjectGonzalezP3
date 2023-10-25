7db39a86bdebd97ef31eccf05ff3d51be5954899
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2023-10-25 10:54:39 -0700

    coded
    Added a code so we can mutliply by 2

---
+++ ScriptingProjectP3Gonzalez/Assets/Scripts/Variables.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+
+    int myInt = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MutiplyBytwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    int MutiplyBytwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
