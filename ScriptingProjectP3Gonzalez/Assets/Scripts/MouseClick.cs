ef1718d1cb96d72935d6d89ba0ae2e378efba1d3
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2024-03-12 11:01:15 -0700

    Made a new scripot
    script

---
+++ ScriptingProjectP3Gonzalez/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseClick : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        rb.AddForce(-transform.forward * 500f);
+        rb.useGravity = true;
+    }
+}
+
+
+
